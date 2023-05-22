6ad0c0ab6f3c6a7817d081eedffc6a3b4d61df29
Jake Cunninghame <jake.cunninghame@gmail.com>
2023-05-22 08:38:29 -0400

    cache and func treatment for data, adding some blurbs

--- main_page.py
+++ main_page.py
@@ -4,63 +4,41 @@ import plost
 import snowflake.connector as sn
 from dotenv import load_dotenv
 import os
+import util
 
 load_dotenv()
 
-st.markdown("# High Level Summary")
-st.sidebar.markdown("# Claims Summary")
-
 # Connect and fetch data
-con = sn.connect(
-    user=os.getenv('SNOWFLAKE_USER'),
-    password=os.getenv('SNOWFLAKE_PASSWORD'),
-    account=os.getenv('SNOWFLAKE_ACCOUNT'),
-    warehouse=os.getenv('SNOWFLAKE_WH'),
-    role=os.getenv('SNOWFLAKE_ROLE')
-)
-cs = con.cursor()
-cs.execute("""SELECT PT.*, PB.MEMBER_COUNT FROM TUVA_PROJECT_DEMO.PMPM.PMPM_TRENDS PT
+conn = util.connection()
+
+@st.cache_data
+def summary_data():
+    query = """SELECT PT.*, PB.MEMBER_COUNT FROM TUVA_PROJECT_DEMO.PMPM.PMPM_TRENDS PT
               LEFT JOIN (SELECT CONCAT(LEFT(YEAR_MONTH, 4), '-', RIGHT(YEAR_MONTH, 2)) AS YEAR_MONTH, 
                          COUNT(*) AS MEMBER_COUNT
                          FROM TUVA_PROJECT_DEMO.PMPM.PMPM_BUILDER
                          GROUP BY YEAR_MONTH) AS PB
-              ON PT.YEAR_MONTH = PB.YEAR_MONTH;""")
+              ON PT.YEAR_MONTH = PB.YEAR_MONTH;"""
 
-data = cs.fetch_pandas_all()
-data['YEAR_MONTH'] = pd.to_datetime(data['YEAR_MONTH'], format='%Y-%m').dt.date
+    data = util.safe_to_pandas(conn, query)
+    data['year_month'] = pd.to_datetime(data['year_month'], format='%Y-%m').dt.date
+    return data
 
-st.markdown("### Summary of Claims")
-start_date, end_date = st.select_slider("Select date range for claims summary",
-                                        options=data['YEAR_MONTH'].sort_values(),
-                                        value=(data['YEAR_MONTH'].min(), data['YEAR_MONTH'].max()))
-filtered_data = data.loc[(data['YEAR_MONTH'] >= start_date) & (data['YEAR_MONTH'] <= end_date), :]
+@st.cache_data
+def gender_data():
+    query = """SELECT GENDER, COUNT(*) AS COUNT FROM TUVA_PROJECT_DEMO.CORE.PATIENT GROUP BY 1;"""
+    data = util.safe_to_pandas(conn, query)
+    return data
 
-# Summary Metrics
-total_spend = filtered_data['MEDICAL_SPEND'].sum()
-total_member_months = filtered_data['MEMBER_MONTH_COUNT'].sum()
-avg_pmpm = total_spend/total_member_months
+@st.cache_data
+def race_data():
+    query = """SELECT RACE, COUNT(*) AS COUNT FROM TUVA_PROJECT_DEMO.CORE.PATIENT GROUP BY 1;"""
+    data = util.safe_to_pandas(conn, query)
+    return data
 
-col1, col2, col3 = st.columns([1.5,1,1])
-col1.metric("Total Medical Spend", '${:,.2f}'.format(total_spend))
-col2.metric("Total Member Months", total_member_months)
-col3.metric("Average PMPM", '${:,.2f}'.format(avg_pmpm))
-
-st.divider()
-y_axis = st.selectbox('Select Metric for Trend Line', [x for x in data.columns if x != 'YEAR_MONTH'])
-
-if y_axis:
-    st.line_chart(filtered_data,  x='YEAR_MONTH', y=y_axis)
-
-# Patient Demographic Section
-st.divider()
-st.subheader('Patient Demographics')
-st.write('Patient data static, not filtered by claims date sliders currently')
-
-cs.execute("""SELECT GENDER, COUNT(*) AS COUNT FROM TUVA_PROJECT_DEMO.CORE.PATIENT GROUP BY 1;""")
-demo_gender = cs.fetch_pandas_all()
-cs.execute("""SELECT RACE, COUNT(*) AS COUNT FROM TUVA_PROJECT_DEMO.CORE.PATIENT GROUP BY 1;""")
-demo_race = cs.fetch_pandas_all()
-cs.execute("""SELECT CASE 
+@st.cache_data
+def age_data():
+    query = """SELECT CASE 
                         WHEN div0(current_date() - BIRTH_DATE, 365) < 49 THEN '34-48'
                         WHEN div0(current_date() - BIRTH_DATE, 365) >= 49 AND div0(current_date() - BIRTH_DATE, 365) < 65 THEN '49-64'
                         WHEN div0(current_date() - BIRTH_DATE, 365) >= 65 AND div0(current_date() - BIRTH_DATE, 365) < 79 THEN '65-78'
@@ -70,23 +48,60 @@ cs.execute("""SELECT CASE
                 COUNT(*) AS COUNT
                 FROM TUVA_PROJECT_DEMO.CORE.PATIENT
                 GROUP BY 1
-                ORDER BY 1;""")
-demo_age = cs.fetch_pandas_all()
+                ORDER BY 1;"""
+    data = util.safe_to_pandas(conn, query)
+    return data
+data = summary_data()
+demo_gender = gender_data()
+demo_race = race_data()
+demo_age = age_data()
+
+st.markdown("# Summary of Claims")
+start_date, end_date = st.select_slider("Select date range for claims summary",
+                                        options=data['year_month'].sort_values(),
+                                        value=(data['year_month'].min(), data['year_month'].max()))
+filtered_data = data.loc[(data['year_month'] >= start_date) & (data['year_month'] <= end_date), :]
+
+st.markdown("### High Level Summary")
+st.markdown("""At a glance, see the total medical spend and PMPM for the chosen time period. As well as a trend
+graph for other important financial metrics""")
+st.sidebar.markdown("# Claims Summary")
+
+# Summary Metrics
+total_spend = filtered_data['medical_spend'].sum()
+total_member_months = filtered_data['member_month_count'].sum()
+avg_pmpm = total_spend/total_member_months
+
+col1, col2, col3 = st.columns([1.5,1,1])
+col1.metric("Total Medical Spend", '${:,.2f}'.format(total_spend))
+col2.metric("Total Member Months", total_member_months)
+col3.metric("Average PMPM", '${:,.2f}'.format(avg_pmpm))
+
+st.divider()
+y_axis = st.selectbox('Select Metric for Trend Line', [x for x in data.columns if x != 'year_month'])
+
+if y_axis:
+    st.line_chart(filtered_data,  x='year_month', y=y_axis)
+
+# Patient Demographic Section
+st.divider()
+st.markdown('### Patient Demographics')
+st.markdown("""The patient population during this claims period was mostly `female`, `white` and largely 
+over the age of 65, with nearly half of patients falling into the `65-78` age group""")
+st.write(' Please note that patient data is static, and not filtered by claims date sliders currently')
 
 demo_col1, demo_col2 = st.columns([1, 2])
 with demo_col1:
-    plost.donut_chart(demo_gender, theta='COUNT',
-                      color=dict(field='GENDER', scale=dict(range=['#F8B7CD', '#67A3D9'])), legend='left',
+    plost.donut_chart(demo_gender, theta='count',
+                      color=dict(field='gender', scale=dict(range=['#F8B7CD', '#67A3D9'])), legend='left',
                       title='Gender Breakdown')
 with demo_col2:
     plost.bar_chart(
-        demo_age, bar='AGE_GROUP', value='COUNT', legend=None, use_container_width=True,
+        demo_age, bar='age_group', value='count', legend=None, use_container_width=True,
         title='Counts by Age Group'
     )
 
 plost.bar_chart(
-    demo_race, bar='RACE', value='COUNT', color='RACE', legend='bottom', use_container_width=True,
-    title='Counts by Race'
+    demo_race, bar='race', value='count', color='race', legend='bottom', use_container_width=True,
+    title='Counts by Race', height=400
 )
-
-con.close()
