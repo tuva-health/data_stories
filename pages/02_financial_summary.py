fba697a361b27cc687512f1c7011466be3856f4a
Jake Cunninghame <jake.cunninghame@gmail.com>
2023-05-23 14:49:04 -0400

    pharm trend line, remove drilldown page

--- pages/02_financial_summary.py
+++ pages/02_financial_summary.py
@@ -181,6 +181,17 @@ start_year, end_year = st.select_slider(
 )
 selected_range = year_values[year_values.index(start_year): year_values.index(end_year)+1]
 
+## --------------------------------- ##
+## --- Pharmacy Spend            --- ##
+## --------------------------------- ##
+st.markdown("## Pharmacy Spend")
+st.markdown("""
+A look at pharmacy spend over time during the claims period selected.
+""")
+pharm_pmpm = pmpm_by_claim_type()
+pharm_pmpm = pharm_pmpm.loc[pharm_pmpm['claim_type'] == 'pharmacy', :]
+pharm_pmpm = pharm_pmpm.loc[pharm_pmpm["year_month"].str[:4].isin(selected_range)]
+st.line_chart(data=pharm_pmpm, x='year_month', y='paid_amount_sum')
 
 ## --------------------------------- ##
 ## ---                           --- ##
